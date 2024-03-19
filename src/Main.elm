dbffbca6d248fdee4209240cbc3e1a4d90cb6a8e
Grimless <kyle@kyleroucis.com>
2024-03-19 13:40:56 -0400

    More complete page?

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,79 @@
-module Main exposing (main)
+module Main exposing (..)
 
-import Html exposing (text)
+import Browser
+import Html exposing (Html, button, div, input, text)
+import Html.Attributes exposing (class, placeholder, type_)
+import Html.Events exposing (onClick, onInput)
 
 
+
+-- MODEL
+
+
+type alias Model =
+    { email : String
+    , password : String
+    }
+
+
+initialModel : Model
+initialModel =
+    { email = ""
+    , password = ""
+    }
+
+
+
+-- UPDATE
+
+
+type Msg
+    = UpdateEmail String
+    | UpdatePassword String
+    | Submit
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        UpdateEmail newEmail ->
+            { model | email = newEmail }
+
+        UpdatePassword newPassword ->
+            { model | password = newPassword }
+
+        Submit ->
+            -- Here you would handle form submission, e.g., send the data to a server
+            model
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ div []
+            [ input [ type_ "email", placeholder "Email", onInput UpdateEmail ] []
+            ]
+        , div []
+            [ input [ type_ "password", placeholder "Password", onInput UpdatePassword ] []
+            ]
+        , div []
+            [ button [ onClick Submit ] [ text "Sign In" ]
+            ]
+        ]
+
+
+
+-- MAIN
+
+
+main : Program () Model Msg
 main =
-    text "Hello!"
+    Browser.sandbox
+        { init = initialModel
+        , update = update
+        , view = view
+        }
